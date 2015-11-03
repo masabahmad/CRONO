98ad7a266e3e4d813dacb37a7a03d46d3c06a12f
Farrukh Hijaz <hijaz.farrukh@gmail.com>
2015-11-02 22:04:07 -0500

    APSP cleanup: 1st pass complete.

--- apsp/apsp.cc
+++ apsp/apsp.cc
@@ -14,14 +14,14 @@
 
 int _W[8][8] =
 {
-  {0,   2,      1,      17,     MAX,    MAX,    MAX,    MAX},
-  {2,   0,      MAX,    MAX,    2,      6,      MAX,    MAX},
-  {1,   MAX,    0,      MAX,    MAX,    MAX,    MAX,    8},
-  {17,  MAX,    MAX,    0,      MAX,    2,      1,      9},
-  {MAX, 2,      MAX,    MAX,    0,      4,      MAX,    MAX},
-  {MAX, 6,      MAX,    2,      4,      0,      5,      MAX},
-  {MAX, MAX,    MAX,    1,      MAX,    5,      0,      3},
-  {MAX, MAX,    8,      9,      MAX,    MAX,    3,      0}
+   {0,   2,      1,      17,     MAX,    MAX,    MAX,    MAX},
+   {2,   0,      MAX,    MAX,    2,      6,      MAX,    MAX},
+   {1,   MAX,    0,      MAX,    MAX,    MAX,    MAX,    8},
+   {17,  MAX,    MAX,    0,      MAX,    2,      1,      9},
+   {MAX, 2,      MAX,    MAX,    0,      4,      MAX,    MAX},
+   {MAX, 6,      MAX,    2,      4,      0,      5,      MAX},
+   {MAX, MAX,    MAX,    1,      MAX,    5,      0,      3},
+   {MAX, MAX,    8,      9,      MAX,    MAX,    3,      0}
 };
 
 int min = INT_MAX;
@@ -31,68 +31,68 @@ pthread_mutex_t locks[4194304];
 int u = 0;
 
 
-  void init_weights(int N, int DEG, int** W, int** W_index)
-  {
-          // Initialize to -1
-          for(int i = 0; i < N; i++)
-                  for(int j = 0; j < DEG; j++)
-                          W_index[i][j]= -1;
-  
-          // Populate Index Array
-          for(int i = 0; i < N; i++)
-          {
-                  int last = 0;
-                  for(int j = 0; j < DEG; j++)
-                  {
-                          if(W_index[i][j] == -1)
-                          {
-                                  int neighbor = i + j;//rand()%(max);
-                                  if(neighbor > last)
-                                  {
-                                          W_index[i][j] = neighbor;
-                                          last = W_index[i][j];
-                                  }
-                                  else
-                                  {
-                                          if(last < (N-1))
-                                          {
-                                                  W_index[i][j] = (last + 1);
-                                                  last = W_index[i][j];
-                                          }
-                                  }
-                          }
-                          else
-                          {
-                                  last = W_index[i][j];
-                          }
-                          if(W_index[i][j]>=N)
-                          {
-                                  W_index[i][j] = N-1;
-                          }
-                  }
-          }
-  
-          // Populate Cost Array
-          for(int i = 0; i < N; i++)
-          {
-                  for(int j = 0; j < DEG; j++)
-                  {
-                          double v = drand48();
-                          /*if(v > 0.8 || W_index[i][j] == -1)
-                          {       W[i][j] = MAX;
-                                  W_index[i][j] = -1;
-                          }
-  
-                          else*/ if(W_index[i][j] == i)
-                                  W[i][j] = 0;
-  
-                          else
-                                  W[i][j] = (int) (v*100) + 1;
-                          //printf("   %d  ",W_index[i][j]);
-                  }
-                  //printf("\n");
-          }
-  }
+void init_weights(int N, int DEG, int** W, int** W_index)
+{
+   // Initialize to -1
+   for(int i = 0; i < N; i++)
+      for(int j = 0; j < DEG; j++)
+         W_index[i][j]= -1;
+
+   // Populate Index Array
+   for(int i = 0; i < N; i++)
+   {
+      int last = 0;
+      for(int j = 0; j < DEG; j++)
+      {
+         if(W_index[i][j] == -1)
+         {
+            int neighbor = i + j;//rand()%(max);
+            if(neighbor > last)
+            {
+               W_index[i][j] = neighbor;
+               last = W_index[i][j];
+            }
+            else
+            {
+               if(last < (N-1))
+               {
+                  W_index[i][j] = (last + 1);
+                  last = W_index[i][j];
+               }
+            }
+         }
+         else
+         {
+            last = W_index[i][j];
+         }
+         if(W_index[i][j]>=N)
+         {
+            W_index[i][j] = N-1;
+         }
+      }
+   }
+
+   // Populate Cost Array
+   for(int i = 0; i < N; i++)
+   {
+      for(int j = 0; j < DEG; j++)
+      {
+         double v = drand48();
+         /*if(v > 0.8 || W_index[i][j] == -1)
+           {       W[i][j] = MAX;
+           W_index[i][j] = -1;
+           }
+
+           else*/ if(W_index[i][j] == i)
+         W[i][j] = 0;
+
+         else
+            W[i][j] = (int) (v*100) + 1;
+         //printf("   %d  ",W_index[i][j]);
+      }
+      //printf("\n");
+   }
+}
 
 
 
@@ -104,19 +104,19 @@ int get_local_min(volatile int* Q, volatile int* D, int start, int stop, int N, 
 
 typedef struct
 {
-  int*      local_min;
-  int*      global_min;
-  int*      Q;
-  int*      D;
-  int**     W;
-  int**     W_index;
-  int*      d_count;
-  int       tid;
-  int       P;
-  int       N;
-  int       DEG;
-  pthread_barrier_t* barrier_total;
-	pthread_barrier_t* barrier;
+   int*      local_min;
+   int*      global_min;
+   int*      Q;
+   int*      D;
+   int**     W;
+   int**     W_index;
+   int*      d_count;
+   int       tid;
+   int       P;
+   int       N;
+   int       DEG;
+   pthread_barrier_t* barrier_total;
+   pthread_barrier_t* barrier;
 } thread_arg_t;
 
 int local_min_buffer[1024];
@@ -134,221 +134,221 @@ pthread_t   thread_handle[1024];
 
 void* do_work(void* args)
 {
-  volatile thread_arg_t* arg = (thread_arg_t*) args;
+   volatile thread_arg_t* arg = (thread_arg_t*) args;
 
-  //volatile int* Q          = arg->Q;
-  //int* D                   = arg->D;
-  int** W                  = arg->W;
-  int** W_index            = arg->W_index;
-  const int N              = arg->N;
-  const int DEG            = arg->DEG;
-  int uu = 0;
-  P_global = start;
+   //volatile int* Q          = arg->Q;
+   //int* D                   = arg->D;
+   int** W                  = arg->W;
+   int** W_index            = arg->W_index;
+   const int N              = arg->N;
+   const int DEG            = arg->DEG;
+   int uu = 0;
+   P_global = start;
 
-  /*int* D;
-  int* Q;
+   /*int* D;
+     int* Q;
 
-  int p0 = posix_memalign((void**) &D, 64, N * sizeof(int));
-  int p1 = posix_memalign((void**) &Q, 64, N * sizeof(int));
+     int p0 = posix_memalign((void**) &D, 64, N * sizeof(int));
+     int p1 = posix_memalign((void**) &Q, 64, N * sizeof(int));
 
-  for(int i=0;i<N;i++)
-  {
-  	
-	D[i] = INT_MAX;
-  	Q[i] = 1;
-  }
-  D[0]=0;*/
+     for(int i=0;i<N;i++)
+     {
 
-  int node = 0;
-
-  pthread_barrier_wait(arg->barrier_total);
-
-  while(node<N)
-  {
-
-     pthread_mutex_lock(&lock); 
-		 next_source++;
-		 node = next_source;
-		 pthread_mutex_unlock(&lock);
-     
-     int *D;
-     int *Q;
-   if (posix_memalign((void**) &D, 64, N * sizeof(int))) 
-   {
-      fprintf(stderr, "Allocation of memory failed\n");
-      exit(EXIT_FAILURE);
-   }
-   if ( posix_memalign((void**) &Q, 64, N * sizeof(int)))
-   {
-      fprintf(stderr, "Allocation of memory failed\n");
-      exit(EXIT_FAILURE);
-   }
-     initialize_single_source(D, Q, node, N);
-
-	   for(uu=0;uu<N;uu++)
-		 {
-        for(int i = 0; i < DEG; i++)
-        {
-          if((D[W_index[uu][i]] > (D[uu] + W[uu][i])))
-            D[W_index[uu][i]] = D[uu] + W[uu][i];
-          
-          Q[uu]=0;// po=u;
-        }
+     D[i] = INT_MAX;
+     Q[i] = 1;
      }
-  }
+     D[0]=0;*/
 
-  pthread_barrier_wait(arg->barrier_total);
+   int node = 0;
 
-  return NULL;
+   pthread_barrier_wait(arg->barrier_total);
+
+   while(node<N)
+   {
+
+      pthread_mutex_lock(&lock); 
+      next_source++;
+      node = next_source;
+      pthread_mutex_unlock(&lock);
+
+      int *D;
+      int *Q;
+      if (posix_memalign((void**) &D, 64, N * sizeof(int))) 
+      {
+         fprintf(stderr, "Allocation of memory failed\n");
+         exit(EXIT_FAILURE);
+      }
+      if ( posix_memalign((void**) &Q, 64, N * sizeof(int)))
+      {
+         fprintf(stderr, "Allocation of memory failed\n");
+         exit(EXIT_FAILURE);
+      }
+      initialize_single_source(D, Q, node, N);
+
+      for(uu=0;uu<N;uu++)
+      {
+         for(int i = 0; i < DEG; i++)
+         {
+            if((D[W_index[uu][i]] > (D[uu] + W[uu][i])))
+               D[W_index[uu][i]] = D[uu] + W[uu][i];
+
+            Q[uu]=0;// po=u;
+         }
+      }
+   }
+
+   pthread_barrier_wait(arg->barrier_total);
+
+   return NULL;
 }
 
 
 int main(int argc, char** argv)
 {
 
-  const int P1 = atoi(argv[1]);
-  const int N = atoi(argv[2]);
-  const int DEG = atoi(argv[3]);
+   const int P1 = atoi(argv[1]);
+   const int N = atoi(argv[2]);
+   const int DEG = atoi(argv[3]);
 
-	int P = 256;
-	start = P1;
-	P = P1;
+   int P = 256;
+   start = P1;
+   P = P1;
 
 
-        if (DEG > N)
-        {
-                fprintf(stderr, "Degree of graph cannot be grater than number of Vertices\n");
-                exit(EXIT_FAILURE);
-        }
+   if (DEG > N)
+   {
+      fprintf(stderr, "Degree of graph cannot be grater than number of Vertices\n");
+      exit(EXIT_FAILURE);
+   }
 
-  //int* D;
-  //int* Q;
-  //posix_memalign((void**) &D, 64, N * sizeof(int));
-  //posix_memalign((void**) &Q, 64, N * sizeof(int));
-  int d_count = N;
-  pthread_barrier_t barrier_total;
-	pthread_barrier_t barrier;
+   //int* D;
+   //int* Q;
+   //posix_memalign((void**) &D, 64, N * sizeof(int));
+   //posix_memalign((void**) &Q, 64, N * sizeof(int));
+   int d_count = N;
+   pthread_barrier_t barrier_total;
+   pthread_barrier_t barrier;
 
-  int** W = (int**) malloc(N*sizeof(int*));
-  int** W_index = (int**) malloc(N*sizeof(int*));
-  for(int i = 0; i < N; i++)
-  {
-    //W[i] = (int *)malloc(sizeof(int)*N);
-    int ret = posix_memalign((void**) &W[i], 64, DEG*sizeof(int));
-    int re1 = posix_memalign((void**) &W_index[i], 64, DEG*sizeof(int));
-    if (ret != 0 || re1!=0)
-    {
-       fprintf(stderr, "Could not allocate memory\n");
-       exit(EXIT_FAILURE);
-    }
-  }
-  init_weights(N, DEG, W, W_index);
-  /*for(int i = 0;i<N;i++)
-  {
-        for(int j = 0;j<N;j++)
-        {
-                printf(" %d ",W[i][j]);
-        }
-        printf("\n");
-  }*/
+   int** W = (int**) malloc(N*sizeof(int*));
+   int** W_index = (int**) malloc(N*sizeof(int*));
+   for(int i = 0; i < N; i++)
+   {
+      //W[i] = (int *)malloc(sizeof(int)*N);
+      int ret = posix_memalign((void**) &W[i], 64, DEG*sizeof(int));
+      int re1 = posix_memalign((void**) &W_index[i], 64, DEG*sizeof(int));
+      if (ret != 0 || re1!=0)
+      {
+         fprintf(stderr, "Could not allocate memory\n");
+         exit(EXIT_FAILURE);
+      }
+   }
+   init_weights(N, DEG, W, W_index);
+   /*for(int i = 0;i<N;i++)
+     {
+     for(int j = 0;j<N;j++)
+     {
+     printf(" %d ",W[i][j]);
+     }
+     printf("\n");
+     }*/
 
-  pthread_barrier_init(&barrier_total, NULL, P1);
-	pthread_barrier_init(&barrier, NULL, P1);
+   pthread_barrier_init(&barrier_total, NULL, P1);
+   pthread_barrier_init(&barrier, NULL, P1);
 
-  pthread_mutex_init(&lock, NULL);
-	
-	for(int i=0; i<2097152; i++)
-		pthread_mutex_init(&locks[i], NULL);
-  
-	//initialize_single_source(D, Q, 0, N);
+   pthread_mutex_init(&lock, NULL);
 
-  for(int j = 0; j < P1; j++) {
-    thread_arg[j].local_min  = local_min_buffer;
-    thread_arg[j].global_min = &global_min_buffer;
-    //thread_arg[j].Q          = Q;
-    //thread_arg[j].D          = D;
-    thread_arg[j].W          = W;
-    thread_arg[j].W_index    = W_index;
-    thread_arg[j].d_count    = &d_count;
-    thread_arg[j].tid        = j;
-    thread_arg[j].P          = P;
-    thread_arg[j].N          = N;
-    thread_arg[j].DEG        = DEG;
-    thread_arg[j].barrier_total = &barrier_total;
-		thread_arg[j].barrier    = &barrier;
-  }
-  struct timespec requestStart, requestEnd;
-  clock_gettime(CLOCK_REALTIME, &requestStart);
+   for(int i=0; i<2097152; i++)
+      pthread_mutex_init(&locks[i], NULL);
 
-  // Enable Graphite performance and energy models
-  //CarbonEnableModels();
+   //initialize_single_source(D, Q, 0, N);
 
-  for(int j = 1; j < P1; j++) {
-    pthread_create(thread_handle+j,
-                   NULL,
-                   do_work,
-                   (void*)&thread_arg[j]);
-  }
-  do_work((void*) &thread_arg[0]);
+   for(int j = 0; j < P1; j++) {
+      thread_arg[j].local_min  = local_min_buffer;
+      thread_arg[j].global_min = &global_min_buffer;
+      //thread_arg[j].Q          = Q;
+      //thread_arg[j].D          = D;
+      thread_arg[j].W          = W;
+      thread_arg[j].W_index    = W_index;
+      thread_arg[j].d_count    = &d_count;
+      thread_arg[j].tid        = j;
+      thread_arg[j].P          = P;
+      thread_arg[j].N          = N;
+      thread_arg[j].DEG        = DEG;
+      thread_arg[j].barrier_total = &barrier_total;
+      thread_arg[j].barrier    = &barrier;
+   }
+   struct timespec requestStart, requestEnd;
+   clock_gettime(CLOCK_REALTIME, &requestStart);
 
-  printf("\nThreads Returned!");
+   // Enable Graphite performance and energy models
+   //CarbonEnableModels();
 
-  for(int j = 1; j < P1; j++) { //mul = mul*2;
-    pthread_join(thread_handle[j],NULL);
-  }
+   for(int j = 1; j < P1; j++) {
+      pthread_create(thread_handle+j,
+            NULL,
+            do_work,
+            (void*)&thread_arg[j]);
+   }
+   do_work((void*) &thread_arg[0]);
 
-  // Disable Graphite performance and energy models
-  //CarbonDisableModels();
+   printf("\nThreads Returned!");
 
-  printf("\nThreads Joined!");
+   for(int j = 1; j < P1; j++) { //mul = mul*2;
+      pthread_join(thread_handle[j],NULL);
+   }
 
-	clock_gettime(CLOCK_REALTIME, &requestEnd);
-	double accum = ( requestEnd.tv_sec - requestStart.tv_sec ) + ( requestEnd.tv_nsec - requestStart.tv_nsec ) / BILLION;
-	printf( "\nTime: %lf seconds\n", accum );
+   // Disable Graphite performance and energy models
+   //CarbonDisableModels();
 
-  //printf("\ndistance:%d \n",D[N-1]);
+   printf("\nThreads Joined!");
 
-    /*for(int i = 0; i < N; i++) {
-      printf(" %d ", D[i]);
-    }
-    printf("\n");
-*/
-  return 0;
+   clock_gettime(CLOCK_REALTIME, &requestEnd);
+   double accum = ( requestEnd.tv_sec - requestStart.tv_sec ) + ( requestEnd.tv_nsec - requestStart.tv_nsec ) / BILLION;
+   printf( "\nTime: %lf seconds\n", accum );
+
+   //printf("\ndistance:%d \n",D[N-1]);
+
+   /*for(int i = 0; i < N; i++) {
+     printf(" %d ", D[i]);
+     }
+     printf("\n");
+     */
+   return 0;
 }
 
 int initialize_single_source(int*  D,
-                             int*  Q,
-                             int   source,
-                             int   N)
+      int*  Q,
+      int   source,
+      int   N)
 {
-  for(int i = 0; i < N+1; i++)
-  {
-    D[i] = INT_MAX;
-    Q[i] = 1;
-  }
+   for(int i = 0; i < N+1; i++)
+   {
+      D[i] = INT_MAX;
+      Q[i] = 1;
+   }
 
-  D[source] = 0;
-  return 0;
+   D[source] = 0;
+   return 0;
 }
 
 int get_local_min(volatile int* Q, volatile int* D, int start, int stop, int N, int** W_index, int** W, int u)
 {
-  int min = INT_MAX;
-  int min_index = N;
+   int min = INT_MAX;
+   int min_index = N;
 
-  for(int i = start; i < stop; i++) 
-  {
-    if(D[i] < min && Q[i]) 
-    {
-      min = D[i];
-      min_index = W_index[u][i];
-    }
-  }
-  return min_index;
+   for(int i = start; i < stop; i++) 
+   {
+      if(D[i] < min && Q[i]) 
+      {
+         min = D[i];
+         min_index = W_index[u][i];
+      }
+   }
+   return min_index;
 }
 
 void relax(int u, int i, volatile int* D, int** W, int** W_index, int N)
 {
-  if((D[W_index[u][i]] > (D[u] + W[u][i]) && (W_index[u][i]!=-1 && W_index[u][i]<N && W[u][i]!=INT_MAX)))
-    D[W_index[u][i]] = D[u] + W[u][i];
+   if((D[W_index[u][i]] > (D[u] + W[u][i]) && (W_index[u][i]!=-1 && W_index[u][i]<N && W[u][i]!=INT_MAX)))
+      D[W_index[u][i]] = D[u] + W[u][i];
 }
